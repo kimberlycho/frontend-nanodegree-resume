a4732dcb49097a7ce10e487586e75f78c883c09d
Kim Cho <kim.cho@eflglobal.com>
2015-11-20 15:16:16 -0500

    work from 11/20 with UDacity

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,3 +1,11 @@
 /*
 This is empty on purpose! Your code to build the resume will go here.
  */
+var name = "Kimberly Cho";
+var formattedName = HTMLheaderName.replace("%data%", name);
+
+var role = "Survey Developer";
+var formattedRole = HTMLheaderRole.replace("%data%", role);
+
+ $("#header").prepend([formattedRole]);
+ $("#header").prepend([formattedName]);
